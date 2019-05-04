8aaa83e5c335c43824279629ed845907f3f6560a
flyceek <flyceek@gmail.co>
2019-05-04 11:27:04 +0800

    update

--- jdk/alpine-jdk.Dockerfile
+++ jdk/alpine-jdk.Dockerfile
@@ -1,53 +1,6 @@
 FROM alpine:latest
 MAINTAINER flyceek <flyceek@gmail.com>
 
-ARG JDK_VER=8 
-ARG JDK_UPDATE=211
-ARG JDK_BUILD=b12
-ARG JDK_URL_ID=478a62b7d4e34b78b671c754eaaf38ab
-ARG JDK_ED=${JDK_VER}u${JDK_UPDATE}
+COPY build.sh /build.sh
 
-ARG JDK_FILE_SAVE_PATH=/opt/soft/java/jdk
-ARG JDK_FILE_NAME=jdk-${JDK_ED}-linux-x64.tar.gz
-ARG JDK_FILE_SHA256=28a00b9400b6913563553e09e8024c286b506d8523334c93ddec6c9ec7e9d346
-ARG JDK_FILE_EXTRACT_DIR=jdk1.${JDK_VER}.0_${JDK_UPDATE}
-ARG JDK_FILE_URL=http://download.oracle.com/otn/java/jdk/${JDK_ED}-${JDK_BUILD}/${JDK_URL_ID}/${JDK_FILE_NAME}
-
-ARG WORK_DIR='/tmp'
-
-ENV GLIBC_VERSION='2.25-r0'
-ARG GLIBC_DOWNLOAD_URL=https://github.com/sgerrand/alpine-pkg-glibc/releases/download/${GLIBC_VERSION}
-ARG GLIBC_FILE_NAME=glibc-${GLIBC_VERSION}.apk
-ARG GLIBC_FILE_URL=${GLIBC_DOWNLOAD_URL}/${GLIBC_FILE_NAME}
-ARG GLIBC_BIN_FILE_NAME=glibc-bin-${GLIBC_VERSION}.apk
-ARG GLIBC_BIN_FILE_URL=${GLIBC_DOWNLOAD_URL}/${GLIBC_BIN_FILE_NAME}
-ARG GLIBC_I18N_FILE_NAME=glibc-i18n-${GLIBC_VERSION}.apk
-ARG GLIBC_I18N_FILE_URL=${GLIBC_DOWNLOAD_URL}/${GLIBC_I18N_FILE_NAME}
-ARG GLIBC_SGERRAND_URL=https://raw.githubusercontent.com/sgerrand/alpine-pkg-glibc/master/sgerrand.rsa.pub
-
-ENV JAVA_HOME=${JDK_FILE_SAVE_PATH}/${JDK_FILE_EXTRACT_DIR}
-ENV JRE_HOME=${JAVA_HOME}/jre
-ENV CLASSPATH=.:${JAVA_HOME}/lib/dt.jar:${JAVA_HOME}/lib/tools.jar
-ENV PATH=${PATH}:${JAVA_HOME}/bin:${JRE_HOME}/bin
-
-RUN apk --update add --no-cache --virtual=.build-dependencies wget ca-certificates unzip \
-    && mkdir -p ${WORK_DIR} \
-    && mkdir -p ${JAVA_HOME} \
-    && cd ${WORK_DIR} \
-    && wget ${GLIBC_SGERRAND_URL} -O /etc/apk/keys/sgerrand.rsa.pub \
-    && wget ${GLIBC_FILE_URL} ${GLIBC_BIN_FILE_URL} ${GLIBC_I18N_FILE_URL} \
-    && apk add --no-cache ${GLIBC_FILE_NAME} ${GLIBC_BIN_FILE_NAME} ${GLIBC_I18N_FILE_NAME} \
-    && wget --no-cookies --no-check-certificate --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" ${JDK_FILE_URL} \
-    && echo "${JDK_FILE_SHA256}  ${JDK_FILE_NAME}" | sha256sum -c - \
-    && tar -xvf ${JDK_FILE_NAME} -C ${JAVA_HOME} --strip-components=1 \
-    && ln -s ${JAVA_HOME}/bin/java /usr/bin/java \
-    && ln -s ${JAVA_HOME}/bin/javac /usr/bin/javac \
-    && ln -s ${JAVA_HOME}/bin/jar /usr/bin/jar \
-    && rm -f ${JAVA_HOME}/*.zip \
-    && rm -fr ${WORK_DIR}/* \
-    && rm -fr /var/cache/apk/* \
-    && rm /etc/apk/keys/sgerrand.rsa.pub \
-    && /usr/glibc-compat/bin/localedef --force --inputfile POSIX --charmap UTF-8 C.UTF-8 || true \
-    && apk --update del glibc-i18n .build-dependencies\
-    && cd / \
-    && echo "root:123321" | chpasswd
+RUN ["sh","/build.sh","alpine","8","211","b12","478a62b7d4e34b78b671c754eaaf38ab","28a00b9400b6913563553e09e8024c286b506d8523334c93ddec6c9ec7e9d346","1556938657_b42227f2d5d7c099174bfc12df032109"]
---
+++ jdk/build.sh
@@ -0,0 +1,137 @@
+#!/bin/sh
+SYSTEM=$1
+JDK_VER=$2
+JDK_UPDATE=$3
+JDK_BUILD=$4
+JDK_URLID=$5
+JDK_SHA256=$6
+AUTH_PARAM=$7
+
+JDK_SAVE_PATH=/opt/soft/java/jdk
+JDK_ED=${JDK_VER}u${JDK_UPDATE}
+JDK_FILE_NAME=jdk-${JDK_ED}-linux-x64.tar.gz
+JDK_FILE_EXTRACT_DIR=jdk1.${JDK_VER}.0_${JDK_UPDATE}
+JAVA_HOME=${JDK_SAVE_PATH}/${JDK_FILE_EXTRACT_DIR}
+JDK_URL=http://download.oracle.com/otn-pub/java/jdk/${JDK_ED}-${JDK_BUILD}/${JDK_URL_ID}/${JDK_FILE_NAME}?AuthParam=${AUTH_PARAM}
+
+if [ -z "$AUTH_PARAM" ]; then
+    echo 'auth param is empty!'
+    exit 1
+fi
+
+function installCentaOSDependencies(){
+    yum update -y
+    yum install -y tar.x86_64 wget
+}
+
+function installAlpineDependencies(){
+    apk --update add --no-cache --virtual=.build-dependencies wget ca-certificates unzip
+}
+
+function setCentaOSSystem(){
+    installCentaOSDependencies
+}
+
+function setAlpineSystem(){
+    installAlpineDependencies
+}
+
+function setSystemUser(){
+    echo "root:123321" | chpasswd
+}
+
+function installJdk(){
+    mkdir -p ${JAVA_HOME}
+    cd ${JDK_SAVE_PATH}
+    wget --no-cookies --no-check-certificate --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" ${JDK_URL} && echo "${JDK_SHA256} ${JDK_FILE_NAME}" | sha256sum -c - 
+    tar -xvf ${JDK_FILE_NAME} -C ${JAVA_HOME} --strip-components=1
+    if [ $? -ne 0 ]; then
+        echo 'something wrong happened !'
+        exit 1
+    fi
+}
+
+function installAlpineJdk(){
+    mkdir /tmp
+    cd /tmp
+    local GLIBC_VERSION='2.25-r0'
+    local GLIBC_DOWNLOAD_URL=https://github.com/sgerrand/alpine-pkg-glibc/releases/download/${GLIBC_VERSION}
+    local GLIBC_FILE_NAME=glibc-${GLIBC_VERSION}.apk
+    local GLIBC_FILE_URL=${GLIBC_DOWNLOAD_URL}/${GLIBC_FILE_NAME}
+    local GLIBC_BIN_FILE_NAME=glibc-bin-${GLIBC_VERSION}.apk
+    local GLIBC_BIN_FILE_URL=${GLIBC_DOWNLOAD_URL}/${GLIBC_BIN_FILE_NAME}
+    local GLIBC_I18N_FILE_NAME=glibc-i18n-${GLIBC_VERSION}.apk
+    local GLIBC_I18N_FILE_URL=${GLIBC_DOWNLOAD_URL}/${GLIBC_I18N_FILE_NAME}
+    local GLIBC_SGERRAND_URL=https://raw.githubusercontent.com/sgerrand/alpine-pkg-glibc/master/sgerrand.rsa.pub
+    wget ${GLIBC_SGERRAND_URL} -O /etc/apk/keys/sgerrand.rsa.pub
+    wget ${GLIBC_FILE_URL} ${GLIBC_BIN_FILE_URL} ${GLIBC_I18N_FILE_URL}
+    apk add --no-cache ${GLIBC_FILE_NAME} ${GLIBC_BIN_FILE_NAME} ${GLIBC_I18N_FILE_NAME}
+
+    installJdk
+    
+}
+
+function setCentosJdk(){
+    alternatives --install /usr/bin/java java ${JAVA_HOME}/bin/java 1
+    alternatives --install /usr/bin/javac javac ${JAVA_HOME}/bin/javac 1
+    alternatives --install /usr/bin/jar jar ${JAVA_HOME}/bin/jar 1
+}
+
+function setJdk(){
+    ln -s ${JAVA_HOME}/bin/java /usr/bin/java
+    ln -s ${JAVA_HOME}/bin/javac /usr/bin/javac 
+    ln -s ${JAVA_HOME}/bin/jar /usr/bin/jar
+}
+
+function clearSystem(){
+    cd ${JDK_SAVE_PATH}
+    rm -f ${JDK_FILE_NAME}
+    rm -f ${JAVA_HOME}/*.zip
+}
+
+function clearAlpineSystem(){
+    clearSystem
+    rm -fr /tmp
+    rm -fr /var/cache/apk/*
+    rm /etc/apk/keys/sgerrand.rsa.pub
+    /usr/glibc-compat/bin/localedef --force --inputfile POSIX --charmap UTF-8 C.UTF-8 || true
+    apk --update del glibc-i18n .build-dependencies
+}
+
+function installAlpine(){
+    setAlpineSystem
+    installAlpineJdk
+    setJdk
+    clearAlpineSystem
+}
+
+function installCentaOS(){
+    setCentaOSSystem
+    installJdk
+    setCentosJdk
+    clearSystem
+}
+
+function doAction(){
+    local system=$1
+    if [ -z "$system" ]; then
+        echo 'system is empty!'
+        exit 1
+    fi
+    case "$system" in
+    "alpine")
+        echo "set alpine system."
+        installAlpine
+        ;;
+    "centos")
+        echo "set centos system."
+        installCentaOS
+        ;;
+    *)
+        echo "system error,please enter!"
+        exit 1
+        ;;
+    esac
+}
+
+doAction
