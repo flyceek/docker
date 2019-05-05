3e224498c8d44d059932c28677eb07aa7c6be650
paranora <yangqun@centaline.com.cn>
2019-05-05 13:55:04 +0800

    update

--- jdk/build.sh
+++ jdk/build.sh
@@ -45,8 +45,8 @@ function installJdk(){
     cd ${JDK_SAVE_PATH}
     local path=$(pwd)
     echo 'begin download jdk in path :'${path}', url :'${JDK_URL}'.'
-    #curl -L -H "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" ${JDK_URL} && echo "${JDK_SHA256} ${JDK_FILE_NAME}" | sha256sum -c - 
-    wget --no-cookies --no-check-certificate --header "Cookie: gpw_e24=https%3A%2F%2Fwww.oracle.com%2Ftechnetwork%2Fjava%2Fjavase%2Fdownloads%2Fjdk8-downloads-2133151.html; oraclelicense=accept-securebackup-cookie;" ${JDK_URL}
+    #curl -O ${JDK_FILE_NAME} -L -H "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" ${JDK_URL} 
+    wget -O ${JDK_FILE_NAME} --no-cookies --no-check-certificate --header "Cookie: gpw_e24=https%3A%2F%2Fwww.oracle.com%2Ftechnetwork%2Fjava%2Fjavase%2Fdownloads%2Fjdk8-downloads-2133151.html; oraclelicense=accept-securebackup-cookie;" ${JDK_URL}
     pwd
     ls -alsh
     echo "${JDK_SHA256} ${JDK_FILE_NAME}" | sha256sum -c - 
--- jdk/centos8-jdk-8u211.Dockerfile
+++ jdk/centos8-jdk-8u211.Dockerfile
@@ -3,4 +3,4 @@ MAINTAINER flyceek <flyceek@gmail.com>
 
 COPY build.sh /build.sh
 
-RUN ["sh","/build.sh","centos","8","211","b12","478a62b7d4e34b78b671c754eaaf38ab","c0b7e45330c3f79750c89de6ee0d949ed4af946849592154874d22abc9c4668d","1557035301_1861fb105a99e550d422ace1a8384c0e"]
+RUN ["sh","/build.sh","centos","8","211","b12","478a62b7d4e34b78b671c754eaaf38ab","c0b7e45330c3f79750c89de6ee0d949ed4af946849592154874d22abc9c4668d","1557035804_341d7d67cf4de115a054608395dc4891"]
