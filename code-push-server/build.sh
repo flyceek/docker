3ddeff3f351da6ea193116769f508a9a9376b72f
paranora <yangqun@centaline.com.cn>
2019-06-18 15:00:44 +0800

    u

---
+++ code-push-server/build.sh
@@ -0,0 +1,115 @@
+#!/bin/sh
+WORK_HOME=/opt/code-push-server
+VERSION=$1
+APP_PATH=''
+
+USER=$2
+
+function installSystemDependencies(){
+    apk add --update --no-cache --virtual=.yapi-dependencies git wget
+}
+
+function setNpm(){
+    npm config set registry https://registry.npm.taobao.org
+}
+
+function installNpmDependencies() {
+    npm i -g pm2@latest --no-optional
+}
+
+function createUserGroup(){
+    if [ -z "$1" ]; then
+        echo 'yapi user is empty!'
+        exit 1
+    fi
+    local user=$1
+    local userId=1090
+    local group=$user
+    local groupId=1090
+    echo 'begin crate user :'$user ', group :'$group'.'
+    addgroup -g ${groupId} ${group}
+    adduser -h /home/${user} -u ${userId} -G ${group} -s /bin/bash -D ${user}
+}
+
+function installCodePushServerByReleaseCode(){
+    local fileName=v${VERSION}.tar.gz
+    local fileUrl=https://github.com/lisong/code-push-server/archive/${fileName}
+    local srcPath=${WORK_HOME}/code-push-server-v${VERSION}
+    
+    mkdir -p ${srcPath}
+    cd ${WORK_HOME}
+    wget ${fileUrl}
+    if [ ! -f "$fileName" ]; then
+        echo 'down file '$fileName' is error !'
+        exit 1
+    fi
+    tar -xzvf ${fileName} -C ${srcPath} --strip-components 1
+    rm ${fileName}
+    cd ${srcPath}
+    echo 'begin npm install in :'${srcPath}' .'
+    npm install --production
+    APP_PATH=${srcPath}
+    if [ $? -ne 0 ]; then
+        echo 'something wrong happened !'
+        exit 1
+    fi
+}
+
+function installCodePushServerBySourceCode(){
+    local gitUrl=https://github.com/lisong/code-push-server.git
+    local srcPath=${WORK_HOME}/code-push-server-source-master
+    mkdir -p ${srcPath}
+    cd ${WORK_HOME}
+    git clone --depth=1 --single-branch --branch=master ${gitUrl} ${srcPath}
+    cd ${srcPath}
+    echo 'begin npm install in :'${srcPath}' .'
+    npm install --production
+    APP_PATH=${srcPath}
+    if [ $? -ne 0 ]; then
+        echo 'something wrong happened !'
+        exit 1
+    fi
+}
+
+function installCodePushServer(){
+    if [ -n "${VERSION}" ]; then
+        echo 'input version is :'${VERSION}' .'
+        echo 'install yapi use release code!'
+        installCodePushServerByReleaseCode
+    else
+        echo 'install yapi use source code!'
+        installCodePushServerBySourceCode
+    fi
+}
+
+function createCodePushServerStartShell(){
+    if [ ! -d "${APP_PATH}" ]; then
+        echo 'app source path : '${APP_PATH}' is not found !'
+        exit 1
+    fi
+
+    echo -e '#!/bin/sh
+cd '${APP_PATH}'
+pm2 start app.js
+pm2 logs'>/usr/local/bin/code-push-server-start
+
+    chmod +x /usr/local/bin/code-push-server-start
+}
+
+function setSystem(){
+    echo "root:123321" | chpasswd
+    chown -R ${USER}:${USER} ${WORK_HOME}
+}
+
+function clearSystem(){
+    rm /build.sh
+}
+
+installSystemDependencies
+setNpm
+installNpmDependencies
+createUserGroup ${YAPI_USER}
+installCodePushServer
+createCodePushServerStartShell
+setSystem
+clearSystem
--- code-push-server/code-push-server.Dockerfile
+++ code-push-server/code-push-server.Dockerfile
@@ -1,21 +1,12 @@
 FROM node:8.11.4-alpine
 MAINTAINER flyceek <flyceek@gmail.com>
 
-COPY ./process.json /process.json
+COPY ./config/process.json /process.json
+COPY ./build.sh /build.sh
 
-ENV CODE_PUSH_SERVER_HOME=/opt/code-push-server
-ARG CODE_PUSH_SERVER_VERSION=0.5.4
-ARG CODE_PUSH_SERVER_GITURL=https://github.com/lisong/code-push-server.git
+RUN ["sh","/build.sh","","codepushserver"]
 
-RUN apk add --update --no-cache --virtual=.update-dependencies git \
-    && npm config set registry https://registry.npm.taobao.org/ \
-    && npm i -g pm2@latest --no-optional \
-    && mkdir -p ${CODE_PUSH_SERVER_HOME} \
-    && cd ${CODE_PUSH_SERVER_HOME} \
-    && git clone --depth=1 --single-branch --branch=master ${CODE_PUSH_SERVER_GITURL} ${CODE_PUSH_SERVER_HOME} \
-    && npm install \
-    && echo "root:123321" | chpasswd
 
+USER codepushserver
 EXPOSE 3000
-WORKDIR ${CODE_PUSH_SERVER_HOME}
-CMD ["pm2-docker", "start", "/process.json"]
+CMD ["code-push-server-start"] 
