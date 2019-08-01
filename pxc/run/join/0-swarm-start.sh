f680f419564c1661d3f07df2795fbb93c4ad4a73
paranora <yangqun@centaline.com.cn>
2019-08-01 16:06:27 +0800

    u

---
+++ pxc/run/join/0-swarm-start.sh
@@ -0,0 +1,20 @@
+### slave & master ###
+vi /lib/systemd/system/docker.service
+
+[Service]
+ExecStart=/usr/bin/dockerd -H tcp://0.0.0.0:2375 -H unix:///var/run/docker.sock 
+
+systemctl daemon-reload 
+systemctl restart docker
+systemctl status docker.service
+
+
+### master ###
+docker swarm init --force-new-cluster --advertise-addr 10.4.99.4
+
+docker swarm join-token worker
+docker swarm join-token manager
+
+
+### slave ###
+docker swarm join --token
--- pxc/run/join/1-pxc-node-start.sh
+++ pxc/run/join/1-pxc-node-start.sh
@@ -7,6 +7,7 @@ docker volume create pxc-v0
 
 docker run -d \
 --rm \
+-p 4567:4567 \
 -p 17331:3306 \
 --name=pxc-node0 \
 -v pxc-v0:/var/lib/mysql \
@@ -15,9 +16,21 @@ docker run -d \
 -e CLUSTER_NAME=pxc-cluster0 \
 -e XTRABACKUP_PASSWORD=123321 \
 --privileged \
+--ip 10.0.0.13 \
 percona/percona-xtradb-cluster
 
 docker logs -f pxc-node0
+
+# docker service create \
+# --replicas 1 \
+# --network pxc-net0 \
+# -p 17331:3306 \
+# --name pxc-node0 \
+# --mount type=volume,source=pxc-v0,destination=/var/lib/mysql \
+# -e MYSQL_ROOT_PASSWORD=123321 \
+# -e CLUSTER_NAME=pxc-cluster0 \
+# -e XTRABACKUP_PASSWORD=123321 \
+# percona/percona-xtradb-cluster
 
 # my.cnf:/etc/my.cnf
 
@@ -30,15 +43,17 @@ docker volume create pxc-v1
 
 docker run \
 --rm \
+-p 4567:4567 \
 -p 17331:3306 \
 --name=pxc-node1 \
 -v pxc-v1:/var/lib/mysql \
 --net=pxc-net0 \
 -e MYSQL_ROOT_PASSWORD=123321 \
--e CLUSTER_JOIN=10.4.99.4 \
+-e CLUSTER_JOIN=10.0.0.13 \
 -e CLUSTER_NAME=pxc-cluster0 \
 -e XTRABACKUP_PASSWORD=123321 \
 --privileged \
+--ip 10.0.0.14 \
 percona/percona-xtradb-cluster
 
 docker logs -f pxc-node1
