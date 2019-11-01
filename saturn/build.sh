ccb10a25a0d0d157ac634b4244b1c0a7efad708a
paranora <yangqun@centaline.com.cn>
2019-11-01 13:58:27 +0800

    u

--- saturn/build.sh
+++ saturn/build.sh
@@ -96,7 +96,6 @@ function install() {
     rm -fr ${FILE_NAME}
     cd ${SRC}
     mvn clean package -Dmaven.javadoc.skip=true -Dmaven.test.skip=true
-    npm -v
     if [ ! -f "${MAKE_DIR}/target/${MAKE_TARGET}" ]; then
         echo 'make target , file :'${MAKE_DIR}'/target/'${MAKE_TARGET}' not found!'
         exit 1010
