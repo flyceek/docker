b21b0cadc331d1424c7b6a07d232ad969d47ad2f
paranora <paranora@163.com>
2019-11-20 23:48:36 +0800

    u

--- elastic-job/build.sh
+++ elastic-job/build.sh
@@ -20,8 +20,19 @@ fi
 
 function installCentOSDependencies(){
     yum update -y
+    yum install -y tar.x86_64 wget github
+    wget http://repos.fedorapeople.org/repos/dchen/apache-maven/epel-apache-maven.repo -O /etc/yum.repos.d/epel-apache-maven.repo
+    yum install -y epel-release
+    bash -c 'cat > /etc/yum.repos.d/wandisco-svn.repo <<EOF
+[WANdiscoSVN]
+name=WANdisco SVN Repo 1.9
+enabled=1
+baseurl=http://opensource.wandisco.com/centos/7/svn-1.9/RPMS/\$basearch/
+gpgcheck=1
+gpgkey=http://opensource.wandisco.com/RPM-GPG-KEY-WANdisco
+EOF'
     yum update systemd
-    yum install -y tar.x86_64 wget git cppunit-devel 
+    yum install -y devtoolset-2-toolchain
     yum groupinstall -y "Development Tools"
     yum install -y python-devel python-six python-virtualenv java-1.8.0-openjdk-devel zlib-devel libcurl-devel openssl-devel cyrus-sasl-devel cyrus-sasl-md5 apr-devel subversion-devel apr-util-devel
 }
@@ -56,13 +67,14 @@ function installMesosCentOS(){
     fi
     echo 'setp 2 install mesos.'
     tar -xvf ${mesos_filename} -C /tmp/mesos --strip-components=1
-    cd /tmp/mesos
-    ./configure --prefix=/usr/local/mesos
-    make –j6
-    make –j6 install
+    mkdir -p /tmp/mesos-build
+    cd /tmp/mesos-build
+    ./tmp/mesos/configure --prefix=/usr/local/mesos
+    make
+    make install
     echo 'setp 3 clean mesos.'
+    rm -fr /tmp/mesos
     cd /tmp
-    rm -fr mesos
 }
 
 function installMaven(){
@@ -88,10 +100,7 @@ function installMaven(){
     rm -fr ${maven_filename}
     echo "setp 2 config maven."
     echo 'MAVEN_HOME='${maven_path}>>/etc/profile
-    echo "export PATH=${MAVEN_HOME}/bin:${PATH}">>/etc/profile
-    pwd
-    ls -alsh ${maven_path}
-    cat /etc/profile
+    echo 'export PATH=${MAVEN_HOME}/bin:${PATH}'>>/etc/profile
     source /etc/profile
 }
 
@@ -101,8 +110,8 @@ function installMavenCentOS(){
 
 function settingUpCentOS(){
     installCentOSDependencies
-    installMesosCentOS
     installMavenCentOS
+    installMesosCentOS
 }
 
 function settingUpAlpine(){
