3cc0cbf5984e062d567d14bf200ddbc4d0a64f75
flyceek <flyceek@gmail.com>
2020-05-15 00:00:45 +0800

    u

--- jdk/build.sh
+++ jdk/build.sh
@@ -171,7 +171,7 @@ function clearCentOSSystem(){
 function clearAlpineSystem(){
     clearSystem
     rm -fr /tmp
-    # rm -fr /var/cache/apk/*
+    rm -fr /var/cache/apk/*
     # rm /etc/apk/keys/sgerrand.rsa.pub
     # /usr/glibc-compat/bin/localedef --force --inputfile POSIX --charmap UTF-8 C.UTF-8 || true
     # apk --update del glibc-i18n .build-dependencies
